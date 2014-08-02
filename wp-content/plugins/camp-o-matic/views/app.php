60ce7d146287792a6e4d1fd19c574afdf34bea33
dsmart <derek@hcg.bz>
2014-08-02 15:23:17 -0400

    Started structure/styling

--- wp-content/plugins/camp-o-matic/views/app.php
+++ wp-content/plugins/camp-o-matic/views/app.php
@@ -6,6 +6,9 @@
     <script src="<?php echo plugins_url( 'js/angular.min.js' , dirname(__FILE__) ); ?>"></script>
     <script src="<?php echo plugins_url( 'js/angular-route.min.js' , dirname(__FILE__) ); ?>"></script>
     <script src="<?php echo plugins_url( 'js/app.js' , dirname(__FILE__) ); ?>"></script>
+	<link rel="stylesheet" href="<?php echo plugins_url( 'css/sessions.css' , dirname(__FILE__) ); ?>">
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/questions.css">
 </head>
 <body>
     <div id="mainView" ng-view>
