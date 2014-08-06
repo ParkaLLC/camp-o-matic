f1fb251ecf57a5815248695c773bb0c76cc957c2
Rocco Tripaldi <tripaldir@gmail.com>
2014-08-05 22:43:24 -0400

    expanding on endpoints

--- wp-content/plugins/camp-o-matic/classes/campomatic_connection.php
+++ wp-content/plugins/camp-o-matic/classes/campomatic_connection.php
@@ -4,17 +4,26 @@
  */
 class Campomatic_Connection {
     public function register_routes( $routes ) {
-        $routes['/campomatic/connect'] = array(
-            array( array( $this, 'create'), WP_JSON_Server::READABLE ),
+        $routes['/campomatic/login/(?P<key>\w+)'] = array(
+            array( array( $this, 'login_user'), WP_JSON_Server::ALLMETHODS ),
         );
 
-        // Add more custom routes here
+        $routes['/campomatic/register'] = array(
+            array( array( $this, 'register_user'), WP_JSON_Server::ALLMETHODS | WP_JSON_Server::ACCEPT_JSON ),
+        );
 
         return $routes;
     }
 
-    public function create() {
+    public function login_user($key) {
         $response = new WP_JSON_Response();
+        $response->set_data($key);
+        return $response;
+    }
+
+    public function register_user($data) {
+        $response = new WP_JSON_Response();
+        $response->set_data($data);
         return $response;
     }
 
